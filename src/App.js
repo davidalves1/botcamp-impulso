80b7784cd20c4c1c6426c8ca2271a3c3729a277a
davidalves1 <alves.david@outlook.com>
2019-04-04 23:10:41 -0300

    Add css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
+import './App.css';
+
 class App extends Component {
   render() {
     return (
-      <h1>Hello!</h1>
+      <button className="btn-botcamp">Entrar</button>
     );
   }
 }
