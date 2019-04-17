db5992a8a9d79825c11b6280446627653f1852ad
davidalves1 <alves.david@outlook.com>
2019-04-16 22:26:52 -0300

    Add logout component

---
+++ src/components/LogoutBotcamp/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+import logout from '../../img/logout.svg';
+
+const LogoutBotcamp = styled.img.attrs({
+    src: logout,
+    alt: 'Logout'
+})`
+
+`;
+
+export default LogoutBotcamp;
